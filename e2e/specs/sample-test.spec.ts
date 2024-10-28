ba791fdcb83c8ec2f5a0cb7987f3d63799002465
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-10-28 22:07:43 +0300

    feat: disable e2e

--- e2e/specs/sample-test.spec.ts
+++
@@ -1,7 +0,0 @@
-// This test is a sample E2E test. You can delete it.
-
-// test('Sample test', async ({ page }) => {
-//   const homePage = new HomePage(page);
-//   await homePage.goto();
-//   await expect(homePage.page.getByRole('link', { name: 'Home' })).toBeVisible();
-// });
