eef8a763ae47bda9bf337017636c537c2463c4b9
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-11-06 07:30:50 +0300

    use useWSR on fetching

--- src/components/billing-status-summary.test.tsx
+++ src/components/billing-status-summary.test.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { useBillingStatus } from '../resources/billing-status.resource';
 import PatientBillingStatusSummary from './billing-status-summary.component';
 import { getDefaultsFromConfigSchema, openmrsFetch, restBaseUrl, useConfig } from '@openmrs/esm-framework';
 import { Config, configSchema } from '../config-schema';
 import { renderWithSwr } from '../tools/test-utils';
+import { mockGroupedLines } from '../../__mocks__';
 
 const mockedUseConfig = jest.mocked(useConfig<Config>);
-
-jest.mock('../resources/billing-status.resource', () => ({
-  useBillingStatus: jest.fn(),
-}));
+const mockedUseBillingStatus = jest.mocked(useBillingStatus);
 
 describe('PatientBillingStatusSummary', () => {
   beforeEach(() => {
@@ -21,7 +18,7 @@ describe('PatientBillingStatusSummary', () => {
   });
 
   it('should display loading state when isLoading is true', () => {
-    (useBillingStatus as jest.Mock).mockImplementation(() => ({
+    mockedUseBillingStatus.mockImplementation(() => ({
       groupedLines: {},
       isLoading: true,
       isValidating: false,
@@ -34,7 +31,7 @@ describe('PatientBillingStatusSummary', () => {
   });
 
   it('should display error state when error is present', () => {
-    (useBillingStatus as jest.Mock).mockImplementation(() => ({
+    mockedUseBillingStatus.mockImplementation(() => ({
       groupedLines: {},
       isLoading: false,
       isValidating: false,
@@ -48,7 +45,7 @@ describe('PatientBillingStatusSummary', () => {
   });
 
   it('should display empty state when there are no billing lines', () => {
-    (useBillingStatus as jest.Mock).mockImplementation(() => ({
+    mockedUseBillingStatus.mockImplementation(() => ({
       groupedLines: {},
       isLoading: false,
       isValidating: false,
@@ -61,38 +58,7 @@ describe('PatientBillingStatusSummary', () => {
   });
 
   it('should display billing status summary correctly', async () => {
-    const user = userEvent.setup();
-
-    const mockGroupedLines = {
-      '2023-05-01': {
-        id: '2023-05-01',
-        visit: { uuid: 'visit-1', startDate: '2023-05-01T00:00:00', endDate: '2023-05-01T23:59:59' },
-        date: '2023-05-01',
-        status: true,
-        lines: [
-          {
-            id: '1',
-            date: '2023-05-01',
-            document: 'Order 001',
-            order: 'Order 1',
-            tags: ['ORDER', 'FULLY_INVOICED', 'PAID', 'NOT_OVERDUE'],
-            displayName: 'Product 1',
-            approved: true,
-          },
-          {
-            id: '2',
-            date: '2023-05-01',
-            document: 'Order 001',
-            order: 'Order 2',
-            tags: ['ORDER', 'PARTIALLY_INVOICED', 'PAID', 'NOT_OVERDUE'],
-            displayName: 'Product 2',
-            approved: true,
-          },
-        ],
-      },
-    };
-
-    (useBillingStatus as jest.Mock).mockImplementation(() => ({
+    mockedUseBillingStatus.mockImplementation(() => ({
       groupedLines: mockGroupedLines,
       isLoading: false,
       isValidating: false,
